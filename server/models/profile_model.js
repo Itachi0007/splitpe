93d55d9bdeae9d516ca7e60ff5300a85df4332d5
JanakRaj <janakraj1998@gmail.com>
2023-11-09 18:46:45 +0530

    Added Google Auth. Pending - Fix DB issue + Twilio setup for /login /signup routes

--- server/models/profile_model.js
+++ server/models/profile_model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 var profileSchema = new mongoose.Schema(
 	{
 		name: {type: String},
+		googleId: { type: String },
 		phone: {type: String, default: "", required: true},
 		email: {type: String, required: true},
 		password: {type: String, required: true, min: 6},
